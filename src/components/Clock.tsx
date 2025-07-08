6dbf804cb83f37f9c822d45df098c77f8f1a3038
elmetal <nm7.pupu@gmail.com>
2025-07-08 17:26:03 +0900

    Remove title for cleaner design

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,17 +1,12 @@
 import { useTime } from '../hooks/useTime';
 import styles from '../styles/Clock.module.scss';
 
-interface ClockProps {
-    title?: string;
-}
-
-const Clock = ({ title = 'デジタル時計' }: ClockProps) => {
+const Clock = () => {
     const { timeString, dateString, dayOfWeek, timezone } = useTime();
 
     return (
         <div className={styles.clockContainer}>
             <div className={styles.clockWrapper}>
-                <h1 className={styles.title}>{title}</h1>
                 <div className={styles.time}>{timeString}</div>
                 <div className={styles.date}>{dateString}</div>
                 <div className={styles.dayOfWeek}>{dayOfWeek}</div>
